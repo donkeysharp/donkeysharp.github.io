1e404df43fed8844074ee949338b423716af6324
Sergio Guillen <sergio.guillen.dev@gmail.com>
2019-12-17 14:32:05 -0400

    Update deployment script to sync from master before making changes

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+function sync_master() {
+  pushd public
+  git pull origin master
+  popd
+}
+
+function build_site() {
+  sync_master
+
+  # Build the project
+  hugo -t cocoa
+
+  pushd public
+  git add .
+
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
+
+  git push origin master
+
+  popd
+}
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Build the project.
-hugo -t cocoa
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
+build_site
